cedd2593bacacc68189171713fb565db87645b13
Wendelin <wendelin.steiner@gmail.com>
2021-11-12 18:13:15 +0100

    added links to header

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,14 +1,34 @@
 import React from "react";
 import Link from "next/link";
 
+import { useAuth } from "../stores/authContext";
+
 function Header() {
+  const { user, isLoading, isError } = useAuth();
+
+  const links = [
+    { label: "Sign Up", href: "/auth/signup" },
+    { label: "Sign In", href: "/auth/signin" },
+    { label: "Sign Out", href: "/auth/signout" },
+  ]
+    .filter((linkConfig) => linkConfig)
+    .map(({ label, href }) => {
+      return (
+        <li key={href}>
+          <Link href={href}>
+            <a>{label}</a>
+          </Link>
+        </li>
+      );
+    });
+
   return (
     <nav className="navbar navbar-light bg-light">
       <Link href="/">
         <a className="navbar-brand">GitTix</a>
       </Link>
       <div className="d-flex justify-content-end">
-        <ul className="nav d-flex align-items-center">Sign out | Sign in/up</ul>
+        <ul className="nav d-flex align-items-center">{links}</ul>
       </div>
     </nav>
   );
