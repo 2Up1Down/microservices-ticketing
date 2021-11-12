1dba98aa3fda9ef3c90002185f3a6cd56c0e2353
Wendelin <wendelin.steiner@gmail.com>
2021-11-12 08:47:46 +0100

    added first ServerSideProp

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,5 +1,35 @@
-const index = () => {
-  return <div>hello worldff</div>;
+import axios from 'axios';
+
+const LandingPage = ({currentUser}) => {
+    console.log('current user: ', currentUser);
+
+    return <h1>hello world</h1>;
 };
 
-export default index;
+export async function getServerSideProps({req}) {
+
+
+    let res = {};
+    try {
+        res = await axios.get(
+            'http://ingress-nginx-controller.ingress-nginx.svc.cluster.local/api/users/currentuser',
+            {
+                withCredentials: true,
+                headers: req.headers,
+            }
+        );
+
+    } catch (error) {
+        return {
+            props: {currentUser: 'null'},
+        };
+    }
+
+    return {
+        props: res.data,
+    };
+}
+
+export default LandingPage;
+
+
