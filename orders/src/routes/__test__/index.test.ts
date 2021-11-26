164e91cef6b7aaa09eef55130db81c187a817ec1
Wendelin <wendelin.steiner@gmail.com>
2021-11-26 15:47:02 +0100

    Fixed bug on index test router handler

---
+++ orders/src/routes/__test__/index.test.ts
@@ -0,0 +1,54 @@
+import request from "supertest";
+
+import { app } from "../../app";
+import { getAuthCookie } from "../../test/setup";
+import { Ticket, TicketDoc } from "../../models/ticket";
+
+const buildTicket = async () => {
+  const ticket = Ticket.build({
+    title: "abcdefg",
+    price: 123,
+  });
+  await ticket.save();
+
+  return ticket;
+};
+
+const buildOrder = async (userCookie: string[], ticket: TicketDoc) => {
+  const response = await request(app)
+    .post("/api/orders")
+    .set("Cookie", userCookie)
+    .send({
+      ticketId: ticket.id,
+    });
+
+  return response;
+};
+
+it("fetches orders for a particular user", async () => {
+  // create three tickets
+  const ticketOne = await buildTicket();
+  const ticketTwo = await buildTicket();
+  const ticketThree = await buildTicket();
+
+  // Create one order as User #1
+  const userOne = getAuthCookie();
+  await buildOrder(userOne, ticketOne);
+
+  // Create two orders as User #2
+  const userTwo = getAuthCookie();
+  const { body: orderOne } = await buildOrder(userTwo, ticketTwo);
+  const { body: orderTwo } = await buildOrder(userTwo, ticketThree);
+
+  // Make request to get orders for User #2
+  const { body: orders } = await request(app)
+    .get("/api/orders")
+    .set("Cookie", userTwo)
+    .send({})
+    .expect(200);
+
+  // Make sure we only got the orders for User #2
+  expect(orders.length).toEqual(2);
+  expect(orders[0].id).toEqual(orderOne.id);
+  expect(orders[1].id).toEqual(orderTwo.id);
+});
