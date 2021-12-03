3515dcfe833a844098175f0b647855d5bd5ebbaf
Wendelin <wendelin.steiner@gmail.com>
2021-12-03 18:21:38 +0100

    added order-created-listener and test

---
+++ tickets/src/events/listener/order-created-listener.ts
@@ -0,0 +1,38 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@wsticketing/common";
+import { queueGroupName } from "./queue-group-name";
+import { Message } from "node-nats-streaming";
+import { Ticket } from "../../models/ticket";
+import { TicketUpdatedPublisher } from "../publishers/ticket-updated-publisher";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    const {
+      id: orderId,
+      ticket: { id: ticketId },
+    } = data;
+
+    const ticket = await Ticket.findById(ticketId);
+
+    if (!ticket) {
+      throw new Error("Ticket not found");
+    }
+
+    // Mark the ticket as being reserved by setting its orderId property
+    ticket.orderId = orderId;
+
+    await ticket.save();
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      price: ticket.price,
+      title: ticket.title,
+      userId: ticket.userId,
+      orderId: ticket.orderId,
+      version: ticket.version,
+    });
+
+    msg.ack();
+  }
+}
