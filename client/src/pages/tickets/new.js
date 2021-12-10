9fd22f292729610e3d68a79cc181703d2356b529
Wendelin <wendelin.steiner@gmail.com>
2021-12-10 10:59:59 +0100

    added new ticket page

--- client/src/pages/tickets/new.js
+++ client/src/pages/tickets/new.js
@@ -1,11 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 import BaseLayout from "../../components/common/base-layout";
 import getCurrentUser from "../../lib/auth";
+import useRequest from "../../hooks/use-request";
+import Error from "../../components/common/error";
 
 const NewTicketPage = ({ currentUser }) => {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const { doRequest, errors } = useRequest({
+    url: "/api/tickets",
+    method: "post",
+    body: {
+      title,
+      price,
+    },
+    onSuccess: (ticket) => console.log(ticket),
+  });
+
+  const onBlur = () => {
+    const value = parseFloat(price);
+    console.log(value);
+
+    if (isNaN(value)) {
+      return;
+    }
+
+    setPrice(value.toFixed(2));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    doRequest();
+  };
+
   return (
     <BaseLayout user={currentUser}>
       <h1>New Ticket</h1>
+      <form onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="">Title</label>
+          <input
+            value={title}
+            className="form-control"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="">Price</label>
+          <input
+            value={price}
+            className="form-control"
+            onBlur={onBlur}
+            onChange={(e) => setPrice(e.target.value)}
+          />
+        </div>
+        <Error errors={errors} />
+        <button className="btn btn-primary">Submit</button>
+      </form>
     </BaseLayout>
   );
 };
